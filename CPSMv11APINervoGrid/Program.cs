66c76ad0b6d734aff2213c6ef8e50c48b011b3f4
François-Xavier Bouffant <fxbouffant@globalsp.com>
2015-03-30 10:43:44 +0200

    Update program to handle error The requests are now called in a specific order defined in FileNames Add more requests and responses

--- CPSMv11APINervoGrid/Program.cs
+++ CPSMv11APINervoGrid/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.IO;
 using System.Net;
 
@@ -7,7 +8,35 @@ namespace CPSMv11APINervoGrid
     class Program
     {
         private const string ApiUrl = "https://pp-admin.cloudforyou.fr/cortexapi/default.aspx";
-        private static readonly NetworkCredential AdminCredentials = new NetworkCredential("username", "password");
+        private static readonly NetworkCredential AdminCredentials = new NetworkCredential("aleksandrs.selickis@nervogrid.com", "LcF,dO=XFNK$");
+        private static readonly List<String> FileNames = new List<string>
+        {
+            "CustomerFindAll",
+            "CustomerFind",
+            "CustomerGet",
+            "CustomerGetProperties",
+            "CustomerCreate",
+            "CustomerModify",
+            "CustomerServicesFindAll",
+            "CustomerProvisionExchange",
+            "CustomerServiceGet",
+            "CustomerModifyExchange",
+            "UserCreate",
+            "UserGet",
+            "UserGetProperties",
+            "UserServicesFindAll",
+            "UserServiceGet",
+            "UserProvisionExchange",
+            "CustomerServiceGet",
+            "CustomerDistributionGroupsGet",
+            "DistributionGroupAddMember",
+            "UserDeprovisionExchange",
+            "UserDeprovision",
+            "UserDelete",
+            "CustomerDeprovisionExchange",
+            "CustomerDeprovision"
+            //"CustomerDelete"
+        };
 
         static void Main(string[] args)
         {
@@ -21,28 +50,59 @@ namespace CPSMv11APINervoGrid
                 Console.WriteLine("Requests folder not found.");
                 return;
             }
-            foreach (string file in Directory.GetFiles("Requests", "*.xml"))
+            foreach (string filename in FileNames)
             {
-                using (StreamReader reader = new StreamReader(file))
-                {
-                    string fileContent = reader.ReadToEnd();
+                string path = String.Format("Requests/{0}.xml", filename);
 
-                    if (!String.IsNullOrEmpty(fileContent))
+                if (File.Exists(path))
+                {
+                    using (StreamReader reader = new StreamReader(path))
                     {
-                        Console.WriteLine("Sending request: {0}", file);
-                        Console.WriteLine("========== Request ==========");
-                        Console.WriteLine(fileContent);
-                        Console.WriteLine("======== End Request ========");
-                        Console.WriteLine("========== Response ==========");
-                        string response = client.UploadString(ApiUrl, fileContent);
-                        Console.WriteLine(response);
-                        Console.WriteLine("======== End Response ========");
-                        Console.WriteLine();
+                        string fileContent = reader.ReadToEnd();
+
+                        if (!String.IsNullOrEmpty(fileContent))
+                        {
+                            Console.WriteLine("Sending request: {0}", filename);
+                            Console.WriteLine("========== Request ==========");
+                            Console.WriteLine(fileContent);
+                            Console.WriteLine("======== End Request ========");
+                            Console.WriteLine("========== Response ==========");
+                            try
+                            {
+                                WriteResponse(client.UploadString(ApiUrl, fileContent), filename);
+                            }
+                            catch (WebException e)
+                            {
+                                if (e.Response != null)
+                                {
+                                    Stream responseStream = e.Response.GetResponseStream();
+                                    if (responseStream != null)
+                                    {
+                                        WriteResponse(new StreamReader(responseStream).ReadToEnd(), filename);
+                                    }
+                                }
+                            }
+                            Console.WriteLine("======== End Response ========");
+                            Console.WriteLine();
+                        }
                     }
                 }
             }
 
             Console.ReadLine();
+        }
+
+        private static void WriteResponse(string response, string filename)
+        {
+            Console.WriteLine(response);
+            
+            if (!Directory.Exists("Responses"))
+                return;
+
+            using (StreamWriter writer = new StreamWriter(String.Format("Responses/{0}.xml", filename), false))
+            {
+                writer.Write(response);
+            }
         }
     }
 }
